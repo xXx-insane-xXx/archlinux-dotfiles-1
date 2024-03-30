082f1251bb77f11b88e858a285a966fc6bf1d1d8
xXxinsanexXx <shreyanshsaha19@proton.me>
2024-03-30 19:28:34 +0530

    Update emacs config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,54 +1,48 @@
+(package-initialize)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(c-basic-offset 4)
  '(custom-enabled-themes '(gruber-darker))
  '(custom-safe-themes
-   '("4c56af497ddf0e30f65a7232a8ee21b3d62a8c332c6b268c81e9ea99b11da0d3" "00445e6f15d31e9afaa23ed0d765850e9cd5e929be5e8e63b114a3346236c44c" "57a29645c35ae5ce1660d5987d3da5869b048477a7801ce7ab57bfb25ce12d3e" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "efcecf09905ff85a7c80025551c657299a4d18c5fcfedd3b2f2b6287e4edd659" "524fa911b70d6b94d71585c9f0c5966fe85fb3a9ddd635362bfabd1a7981a307" "285d1bf306091644fb49993341e0ad8bafe57130d9981b680c1dbd974475c5c7" "830877f4aab227556548dc0a28bf395d0abe0e3a0ab95455731c9ea5ab5fe4e1" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "18cf5d20a45ea1dff2e2ffd6fbcd15082f9aa9705011a3929e77129a971d1cb3" "d89e15a34261019eec9072575d8a924185c27d3da64899905f8548cbd9491a36" "3e200d49451ec4b8baa068c989e7fba2a97646091fd555eca0ee5a1386d56077" "51ec7bfa54adf5fff5d466248ea6431097f5a18224788d0bd7eb1257a4f7b773" "833ddce3314a4e28411edf3c6efde468f6f2616fc31e17a62587d6a9255f4633" "fee7287586b17efbfda432f05539b58e86e059e78006ce9237b8732fde991b4c" "7f1d414afda803f3244c6fb4c2c64bea44dac040ed3731ec9d75275b9e831fe5" "e27c9668d7eddf75373fa6b07475ae2d6892185f07ebed037eedf783318761d7" default))
+   '("81f53ee9ddd3f8559f94c127c9327d578e264c574cda7c6d9daddaec226f87bb" "e4a702e262c3e3501dfe25091621fe12cd63c7845221687e36a79e17cf3a67e0" "eab123a5ed21463c780e17fc44f9ffc3e501655b966729a2d5a2072832abd3ac" "7fd8b914e340283c189980cd1883dbdef67080ad1a3a9cc3df864ca53bdc89cf" "e27c9668d7eddf75373fa6b07475ae2d6892185f07ebed037eedf783318761d7" "456697e914823ee45365b843c89fbc79191fdbaff471b29aad9dcbe0ee1d5641" "37b6695bae243145fa2dfb41440c204cd22833c25cd1993b0f258905b9e65577" "00cec71d41047ebabeb310a325c365d5bc4b7fab0a681a2a108d32fb161b4006" "10e5d4cc0f67ed5cafac0f4252093d2119ee8b8cb449e7053273453c1a1eb7cc" "d445c7b530713eac282ecdeea07a8fa59692c83045bf84dd112dd738c7bcad1d" "dccf4a8f1aaf5f24d2ab63af1aa75fd9d535c83377f8e26380162e888be0c6a9" "b5fd9c7429d52190235f2383e47d340d7ff769f141cd8f9e7a4629a81abc6b19" "014cb63097fc7dbda3edf53eb09802237961cbb4c9e9abd705f23b86511b0a69" "e9d47d6d41e42a8313c81995a60b2af6588e9f01a1cf19ca42669a7ffd5c2fde" default))
+ '(display-line-numbers 'relative)
+ '(global-hl-indent-scope-mode t)
+ '(global-visual-line-mode nil)
+ '(global-word-wrap-whitespace-mode nil)
+ '(highlight-indent-guides-auto-enabled nil)
+ '(highlight-indent-guides-method 'bitmap)
+ '(ido-enable-flex-matching t)
+ '(ido-mode 'both nil (ido))
+ '(nyan-animate-nyancat t)
+ '(nyan-wavy-trail t)
+ '(package-archives
+   '(("gnu" . "https://elpa.gnu.org/packages/")
+     ("nongnu" . "https://elpa.nongnu.org/nongnu/")
+     ("melpa" . "https://melpa.org/packages/")))
  '(package-selected-packages
-   '(python-mode solarized-theme web-mode emmet-mode autothemer))
- '(tool-bar-mode nil))
+   '(ace-mc emmet-mode nyan-mode doom-themes gruber-darker-theme spacemacs-theme zenburn-theme gruvbox-theme solarized-theme org cyberpunk-theme))
+ '(python-indent-def-block-scale 4)
+ '(tool-bar-mode nil)
+ '(truncate-lines t)
+ '(uniquify-buffer-name-style 'post-forward nil (uniquify))
+ '(word-wrap t)
+ '(word-wrap-by-category t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Iosevka Fixed" :foundry "UKWN" :slant normal :weight regular :height 143 :width normal)))))
-
-
-;; ---- MELPA setup---- ;;
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
-;; and `package-pinned-packages`. Most users will not need or want to do this.
-;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-(package-initialize)
-
-(add-to-list 'package-archives
-             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+ '(default ((t (:inherit nil :extend nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 143 :width normal :foundry "UKWN" :family "Iosevka Fixed"))))
+ '(highlight-indent-guides-character-face ((t nil)))
+ '(highlight-indent-guides-odd-face ((t nil)))
+ '(hl-indent-scope-even-face ((t (:background "#414141"))))
+ '(hl-indent-scope-odd-face ((t (:background "#2c2c2c")))))
 
 
 
-;; ----disable menu bar---- ;;
-(tool-bar-mode -1)
-
-;; ----disable scroll bar---- ;;
-(scroll-bar-mode -1)
-
-;; ----set "gnu" style indenting for c---- ;;
-(setq c-default-style "linux"
-     c-basic-offset 4)
-
-;; ----turn on electric pair mode---- ;;
-(electric-pair-mode 1)
-
-;; ---- manual custom themes---- ;;
-(add-to-list 'custom-theme-load-path
-             "~/.emacs.d/themes/")
-
-(load-theme 'zenburn t)
-
-
-;; ----display line numbers
-(setq display-line-numbers relative)
+;; NOTES
+;; Packages get initialized after .init.el is loaded !!
